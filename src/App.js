662a5b29b24ee3fdeb117613f0d4581961b5311d
EduardoGomez1701 <edojose046@hotmail.com>
2025-04-02 19:37:44 -0500

    Finish

--- src/App.js
+++ src/App.js
@@ -23,14 +23,14 @@ const App = () => {
       </div>
       {/* Footer con enlaces a redes sociales */}
       <footer className="footer">
-        <p>&copy; 2025 Cafe 1543. Todos los derechos reservados.</p>
+        <p><b><i>&copy; 2025 <br></br>1543 Cafe. Todos los derechos reservados.</i></b></p>
         <div className="social-links">
-          <p>Pedide a domicilio</p>
+          <p><b>Escribenos y haz tu pedido a domicilio</b></p>
           {/* Enlace a WhatsApp */}
           <a href="https://wa.me/+573008079009" target="_blank" rel="noopener noreferrer">WhatsApp</a>
-          <p>Visita nuestro perfil en Instagram</p>
+          <p><b>Visita nuestro perfil de Instagram</b></p>
           {/* Enlace a Instagram */}
-          <a href="https://www.instagram.com/1543cafebarstq/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://www.instagram.com/1543cafebarstq/" target="_blank" rel="noopener noreferrer">Instagram</a> <br></br>
           {/* Enlace a WhatsApp <p>Aqui podras ver nuestro Menu</p>
           <a href="https://Menu/" target="_blank" rel="noopener noreferrer">Menu1543</a> */}
         </div>
