b6ca9ff13b798b5def29d1a81f7f68b3ac3023f9
EduardoGomez1701 <edojose046@hotmail.com>
2025-04-15 12:42:34 -0500

    Update

--- src/components/OrderAlert.jsx
+++ src/components/OrderAlert.jsx
@@ -13,7 +13,7 @@ const OrderAlert = ({ selectedTable }) => {
         ? `Hola, el cliente en la mesa ${selectedTable} está listo para hacer su pedido.`
         : `El cliente en la mesa ${selectedTable} te necesita. Corre!. Este es el aviso número ${alertCount + 1}.`;
 
-    const whatsappUrl = `https://wa.me/573225904819?text=${encodeURIComponent(message)}`; // Cambia el número al del mesero
+    const whatsappUrl = `https://wa.me/573216265602?text=${encodeURIComponent(message)}`; // Cambia el número al del mesero
     window.open(whatsappUrl, '_blank'); // Abre WhatsApp en una nueva pestaña
   };
 
